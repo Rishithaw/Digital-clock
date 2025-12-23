b44bf62c21b4670f958d12c142ea9aab6ab7bd41
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-12-23 16:30:55 -0600

    Added functionality

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -1,10 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
 function DigitalClock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  function formatTime() {
+    let hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const meridiem = hours >= 12 ? 'PM' : 'AM';
+
+    hours = hours % 12 || 12;
+
+    return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
+  }
+
+  function padZero(num) {
+    return (num < 10 ? "0" : "") + num;
+  }
+
   return (
     <div className='clock-container'>
       <div className='clock'>
-        <span>00:00:00</span>
+        <span>{formatTime(time)}</span>
       </div>
     </div>
   );
