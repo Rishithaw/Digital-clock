307c828481d409e9ff52ce78e05a5d1606a9dd78
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-12-23 16:12:24 -0600

    Added HTML

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 function DigitalClock() {
   return (
-    <div>
-
+    <div className='clock-container'>
+      <div className='clock'>
+        <span>00:00:00</span>
+      </div>
     </div>
   );
 }
